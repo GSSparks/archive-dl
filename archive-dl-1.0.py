c121ce9a0ff275edb8fe4b130bb10588438e022d
Gary Sparks <garypriscillasparks@gmail.com>
2022-10-09 11:06:01 -0400

    Update - added features

--- archive-dl-1.0.py
+++ archive-dl-1.0.py
@@ -284,7 +284,7 @@ def streamonlypage(url):
         print('Unable to find file data')
         return
     findfile_soup= BeautifulSoup(findfile.text, 'xml')
-    findfile_file= findfile_soup.find('file', {'name': re.compile(r'3ds$|a78$|bin$|chd$|cso$|gba$|gb$|gbc$|iso$|mp3$|64$|nes$|sfc$|wad$|wbfs$|zip$')})
+    findfile_file= findfile_soup.find('file', {'name': re.compile(r'3ds$|a78$|bin$|chd$|cso$|gba$|gb$|gbc$|iso$|64$|nes$|sfc$|wad$|wbfs$|zip$')})
     filename = (findfile_file.get('name'))
 
     url = 'https://archive.org/download/'+game+'/'+filename
